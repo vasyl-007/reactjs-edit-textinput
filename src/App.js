c2a9cf7b7b23aed33d234221ce16d67880e733c6
Vasyl-007 <vasyl.manyo@gmail.com>
2020-04-29 01:39:23 +0300

    React JS edit textInput

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    value: "some editable text",
+    isEditable: false,
+  };
+  changeEditMode = () => {
+    console.log("change edit mode");
+    this.setState({
+      isEditable: !this.state.isEditable,
+    });
+  };
+  updateComponentValue = () => {
+    this.setState({
+      isEditable: false,
+      value: this.refs.theTextInput.value,
+    });
+  };
+  renderEditView = () => {
+    return (
+      <div>
+        <input type="text" defaultValue={this.state.value} ref="theTextInput" />
+        <button onClick={this.changeEditMode}>Cancel</button>
+        <button onClick={this.updateComponentValue}>Save</button>
+      </div>
+    );
+  };
+
+  renderDefaultView = () => {
+    return <div onDoubleClick={this.changeEditMode}>{this.state.value}</div>;
+  };
+  render() {
+    return this.state.isEditable
+      ? this.renderEditView()
+      : this.renderDefaultView();
+  }
+}
+
+export default App;
